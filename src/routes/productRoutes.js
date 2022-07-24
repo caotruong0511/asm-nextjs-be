f7f17e1e8f0e8d4b61bdf0a76160a5b65fcc7955
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-07-24 14:43:16 +0700

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,10 +3,6 @@ import { create, list, read, remove, update } from "../controllers/slider";
 
 const router = Router();
 
-router.get("/products", async (req, res) => {
-  res.json(123);
-});
-
 router.get("/slider",list);
 router.get("/slider/:id",read);
 router.delete("/slider/:id",remove);
