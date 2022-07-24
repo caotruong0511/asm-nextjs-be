162fdedaff2b6af42c92f0cc831bf137d98f6104
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-07-24 14:39:42 +0700

    T2-23-api-create-api-slider

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import { create, list, read, remove, update } from "../controllers/slider";
 
 const router = Router();
 
 router.get("/products", async (req, res) => {
   res.json(123);
 });
+
+router.get("/slider",list);
+router.get("/slider/:id",read);
+router.delete("/slider/:id",remove);
+router.post("/slider",create);
+router.put("/slider/:id",update);
+
 
 module.exports = router;
