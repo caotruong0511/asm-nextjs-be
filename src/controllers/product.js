92b6e838d6e2914423de15cf6f81004d9507c149
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-07-31 15:16:11 +0700

    update api

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -16,7 +16,7 @@ export const create = async (req, res) => {
 };
 export const list = async (req, res) => {
   try {
-    const products = await Product.find({}).populate("catygoryId").exec();
+    const products = await Product.find({}).populate("catygoryId").sort("-createdAt").exec();
     res.json(products);
   } catch (error) {
     res.status(400).json({
@@ -78,6 +78,34 @@ export const getComment = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       message: "Không hiển thị được danh sách",
+    });
+  }
+};
+
+export const getRelated = async (req, res) => {
+  try {
+    const { slug } = req.params;
+    const product = await Product.findOne({ slug }).exec();
+    const productRelated = await Product.find({ slug: { $ne: slug }, catygoryId: product.catygoryId })
+      .limit(4)
+      .sort("-createdAt")
+      .exec();
+
+    res.json(productRelated);
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+};
+
+export const getBySlug = async (req, res) => {
+  try {
+    const products = await Product.findOne({ slug: req.params.slug }).exec();
+    res.json(products);
+  } catch (error) {
+    res.status(400).json({
+      message: "khong hien thi",
     });
   }
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { create, getComment, list, read, remove, search, update } from "../controllers/product";
+import { create, getComment, list, read, remove, search, update, getRelated, getBySlug } from "../controllers/product";
 import { isAdmin, isAuth, requireSignin } from "../middlewares/checkAuth";
 
 const router = Router();
 
 router.get("/product", list);
 router.get("/product/:id/comment", getComment);
+router.get("/product/:slug/getRelated", getRelated);
+router.get("/product/:slug/getBySlug", getBySlug);
 router.get("/product/search", search);
 router.get("/product/:id", read);
 router.delete("/product/:id", requireSignin, isAuth, isAdmin, remove);
