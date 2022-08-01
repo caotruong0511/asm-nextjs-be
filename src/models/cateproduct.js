354844dd686d4bc6882c2ef0f7281650eefe0b37
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-08-01 11:28:58 +0700

    update api cateproduct

--- src/models/cateproduct.js
+++ src/models/cateproduct.js
@@ -10,9 +10,23 @@ const cateproductSchema = new Schema(
       type: String,
       require: true,
     },
-    slug:{
-        type:String
-    }
-},{timestamps:true})
+    slug: {
+      type: String,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+cateproductSchema.pre(/^find/, function (next) {
+  this.populate("products");
+
+  next();
+});
+
+cateproductSchema.virtual("products", {
+  ref: "Product",
+  foreignField: "catygoryId",
+  localField: "_id",
+});
 
 export default mongoose.model("Cateproduct", cateproductSchema);
